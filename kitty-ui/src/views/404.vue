16b06efd80792c904bd33ee05cc40870d58b0025
sweetyaya <shylonegirl@qq.com>
2020-10-13 16:50:27 +0800

    master

---
+++ kitty-ui/src/views/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="site-wrapper site-page--not-found">
+    <div class="site-content__wrapper">
+      <div class="site-content">
+        <h2 class="not-found-title">404</h2>
+        <p class="not-found-desc">抱歉！您访问的d页面<em>失联</em>啦 ...</p>
+              </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
---
+++ kitty-ui/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page">
+    <h2>Home Page</h2>
+    <el-button type="primary" @click="getUser()">获取用户</el-button>
+    <el-button type="primary" @click="getMenu()">获取菜单</el-button>
+  </div>
+</template>
+
+<script>
+/*
+import axios from 'axios'
+import mock from '@/mock/mock.js'
+export default {
+  name: 'Home',
+  methods: {
+    getUser() {
+      axios.get('http://localhost:8080/user').then(function (res) {
+        alert(JSON.stringify(res.data));
+      }).catch(function (err) {
+        alert(err);
+      });
+    },
+    getMenu() {
+      axios.get('http://localhost:8080/menu').then(function (res) {
+        alert(JSON.stringify(res.data));
+      }).catch(function (err) {
+        alert(err);
+      });
+    }
+  }
+}
+*/
+</script>

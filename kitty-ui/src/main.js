1c0f6e1b77a246046b494be67945b7cac425578f
sweetyaya <shylonegirl@qq.com>
2020-10-14 10:46:06 +0800

    master

--- kitty-ui/src/main.js
+++ kitty-ui/src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import api from './http/index'
-
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
@@ -16,6 +16,7 @@ Vue.use(ElementUI)
 Vue.use(api)
 new Vue({
   el: '#app',
+  i18n,
   router,
   render: h=> h(App)
   //components: { App },
--- kitty-ui/src/router/index.js
+++ kitty-ui/src/router/index.js
@@ -3,14 +3,23 @@ import Router from 'vue-router'
 import Login from '@/views/Login'
 import Home from '@/views/Home'
 import NotFound from '@/views/404'
+
+import Main from '@/views/Main'
+import User from '@/views/User'
+import Menu from '@/views/Menu'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      children: [
+        { path: '/main', component: Main, name: '系统介绍'},
+        { path: '/user', component: User, name: '用户管理'},
+        { path: '/menu', component: Menu, name: '菜单管理'},
+      ]
     },
     {
       path: '/login',
@@ -24,3 +33,29 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 登录界面登录成功之后，会把用户信息保存在会话
+  // 存在时间为会话生命周期，页面关闭即失效。
+  let user = sessionStorage.getItem('user')
+  //let userName = sessionStorage.getItem('user')
+  if (to.path === '/login') {
+    // 如果是访问登录界面，如果用户会话信息存在，代表已登录过，跳转到主页
+    if(user) {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+  } else {
+    if (!user) {
+      // 如果访问非登录界面，且户会话信息不存在，代表未登录，则跳转到登录界面
+      next({ path: '/login' })
+    } else {
+      // 加载动态菜单和路由
+      //addDynamicMenuAndRoutes(userName, to, from)
+      next()
+    }
+  }
+})
+
+export default router
